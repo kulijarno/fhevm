ffd3b7e02d7b8545e58ceb14e4745bff2dae2948
Georgi <georgi.goranov@zama.ai>
2025-10-01 13:10:27 +0300

    fix(coprocessor): update sqlx cache

--- coprocessor/fhevm-engine/gw-listener/src/gw_listener.rs
+++ coprocessor/fhevm-engine/gw-listener/src/gw_listener.rs
@@ -189,7 +189,7 @@ impl<P: Provider<Ethereum> + Clone + 'static, A: AwsS3Interface> GatewayListener
             .await?;
 
         let _ = telemetry::try_begin_transaction(
-            &db_pool,
+            db_pool,
             chain_id as i64,
             &transaction_id,
             log.block_number.unwrap_or_default(),
